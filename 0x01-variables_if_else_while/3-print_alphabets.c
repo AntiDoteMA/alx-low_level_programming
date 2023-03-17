baaa5ba88ba8cb67ef7f1f2bfe27af8f2cc2dde0
AntiDoteMA <samir.elaissaoui@gmail.com>
2023-03-17 19:45:20 +0100

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: check if n is positive or negativ or zero.
+ *
+ * Return: Always 0 (Succesful)
+*/
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 65; i <= 90; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
