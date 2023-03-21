bb8557dedd374db05df446704ff8e09707d0a11b
AntiDoteMA <samir.elaissaoui@gmail.com>
2023-03-21 21:52:32 +0000

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,14 +11,15 @@
 
 void print_alphabet_x10(void)
 {
-	int i = 1;
+	int i = 0;
 
-	while (i <= 10)
+	while (i < 10)
 	{
-		for (i = 97; i <= 122; i++)
+		for (j = 97; j <= 122; j++)
 		{
 			_putchar(i);
 		}
+		i++;
 		_putchar('\n');
 	}
 
