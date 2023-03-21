cf9f978d36afc413de15f6beb7d6a2b6c03b4e22
AntiDoteMA <samir.elaissaoui@gmail.com>
2023-03-21 22:12:28 +0000

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+
+/**
+ * _islower - Entry point
+ *
+ * Description: checks for lowercase character.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _islower(int i)
+{
+	if (i < 122 && i > 97)
+	{
+		return(1);
+	}else
+	{
+		return(0);
+	}
+
+}
