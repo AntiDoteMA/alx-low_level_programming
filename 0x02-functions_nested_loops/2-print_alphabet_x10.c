fe5891397df9f7027a2eae02d6ab91fa50d10d04
AntiDoteMA <samir.elaissaoui@gmail.com>
2023-03-21 21:42:02 +0000

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: prints the alphabet, in lowercase 10 times
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	while (i >= 10)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+
+}
