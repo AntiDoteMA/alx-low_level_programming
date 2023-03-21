a60203a07c8bcf6550e4c918b30b6b923aa6562a
AntiDoteMA <samir.elaissaoui@gmail.com>
2023-03-21 22:58:20 +0000

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,14 +16,14 @@ int print_sign(int i)
 {
 	if (i > 0)
 	{
-		_putchar('+');
+		_putchar(0 + 43);
 		return (1);
 	} else if (i < 0)
 	{
-		_putchar('-');
+		_putchar(0 + 48);
 		return (0);
 	}
-	_putchar('0');
+	_putchar(0 + 45);
 	return (-1);
 
 }
