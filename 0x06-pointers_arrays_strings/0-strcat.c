5e530983c2c30bd7c6a9dd1e49c8c6719a2366d4
AntiDoteMA <samir.elaissaoui@gmail.com>
2023-03-31 01:32:28 +0000

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+/**
+ * _strcat - concatenates two strings src and dest
+ *
+ * @src: source of string
+ * @dest: destination of string
+ *
+ * Return: concatenates string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
