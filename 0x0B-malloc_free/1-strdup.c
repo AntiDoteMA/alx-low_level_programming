ed24f36b0d8e30b641d14a6ee23738209ab78165
AntiDoteMA <samir.elaissaoui@gmail.com>
2023-04-11 18:00:27 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,28 +12,22 @@
 
 char *_strdup(char *str)
 {
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-
+	char *dup;
 	int i, len = 0;
 
-	while (str[len] != '\0')
-	{
-		len++;
-	}
-
-	char *dup = (char *) malloc((len + 1) * sizeof(char));
-
-	if (dup == NULL)
-	{
+	if (str == NULL)
 		return (NULL);
-	}
 
-	for (i = 0; i <= len; i++)
-	{
+	while (str[len] != '\0')
+		len++;
+
+	dup = (char *)malloc((sizeof(char) * len) + 1);
+	if (dup == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
 		dup[i] = str[i];
-	}
+	dup[len] = '\0';
+
 	return (dup);
 }
